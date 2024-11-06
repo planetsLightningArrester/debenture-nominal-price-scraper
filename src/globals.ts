a881d2f180e1afb8c553613a36d1780ef5ac47cd
Planets Lightning Arrester <franciscogssm94@gmail.com>
2024-11-06 20:22:02 -0300

    feat(chore): add fallback for the default database

--- src/globals.ts
+++ src/globals.ts
@@ -10,12 +10,31 @@ export interface Asset {
   value: string
 }
 
+/** Error due to scrap issues */
+export class ScrapError extends Error {
+  /** The asset code that was trying to be retrieved when the error happened */
+  assetCode: string
+
+  /**
+   * Create a new `ScrapError`
+   * @param message the error message
+   * @param assetCode the code of the asset
+   */
+  constructor(message: string, assetCode: string) {
+    super(message)
+    this.assetCode = assetCode
+  }
+}
+
+/** Log infos */
 export const info = Print.create()
 info.preAppend(`[${color.cyan}INFO${color.reset}] `)
 info.showDate()
+/** Log warnings */
 export const warn = Print.create()
 warn.preAppend(`[${color.yellow}WARN${color.reset}] `)
 warn.showDate()
+/** Log errors */
 export const err = Print.create()
 err.preAppend(`[${color.red}ERRO${color.reset}] `)
 err.showDate()
--- src/scraper.ts
+++ src/scraper.ts
@@ -1,16 +1,19 @@
 import { parseFromString } from 'dom-parser'
 import { request } from 'gaxios'
 import { DateTime } from 'luxon'
-import { type Asset, info, warn, err } from './globals'
+import puppeteer from 'puppeteer'
+import { type Asset, err, info, ScrapError, warn } from './globals'
 
 /**
  * Updates assets using puppeteer
  * @param assets the assets to update
  * @returns whether any assets was updated
  */
-export async function updateAssets(assets: Asset[]): Promise<[boolean, Error[]]> {
-  // return await puppeteerScraper(assets)
-  return await gaxiosScraper(assets)
+export async function updateAssets(assets: Asset[]): Promise<[boolean, ScrapError[]]> {
+  const [dataUpdated, errors] = await gaxiosScraper(assets)
+  if (errors.length > 0) {
+    return await puppeteerScraper(assets.filter(a => errors.find(e => e.assetCode === a.code)))
+  } else return [dataUpdated, errors]
 }
 
 /**
@@ -18,78 +21,97 @@ export async function updateAssets(assets: Asset[]): Promise<[boolean, Error[]]>
  * @param assets the assets to update
  * @returns whether any assets was updated
  */
-// async function puppeteerScraper(assets: Asset[]): Promise<boolean> {
-//   const today = DateTime.now().setZone('America/New_York').toFormat('yyyy-MM-dd')
-//   if (typeof assets.find(requiredAsset => requiredAsset.refDate !== today) === 'undefined') {
-//     warn.log('All assets are already up-to-date')
-//     return false
-//   }
+async function puppeteerScraper(assets: Asset[]): Promise<[boolean, ScrapError[]]> {
+  const today = DateTime.now().setZone('America/New_York').toFormat('yyyy-MM-dd')
+  if (typeof assets.find(requiredAsset => requiredAsset.refDate !== today) === 'undefined') {
+    warn.log('All assets are already up-to-date')
+    return [false, []]
+  }
 
-//   const browser = await puppeteer.launch()
-//   const page = await browser.newPage()
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
 
-//   const baseUrl = atob(atob('YUhSMGNITTZMeTlrWVhSaExtRnVZbWx0WVM1amIyMHVZbkl2WkdWaVpXNTBkWEpsY3c9PQ'))
+  const debenturesBaseUrl = atob(atob('YUhSMGNITTZMeTlrWVhSaExtRnVZbWx0WVM1amIyMHVZbkl2WkdWaVpXNTBkWEpsY3c9PQ'))
+  const certificateOfReceivableBaseUrl = atob(atob('YUhSMGNITTZMeTlrWVhSaExtRnVZbWx0WVM1amIyMHVZbkl2WTJWeWRHbG1hV05oWkc4dFpHVXRjbVZqWldKcGRtVnBjdw'))
 
-//   let somethingChanged = false
-//   for await (const requiredAsset of assets) {
-//     if (typeof requiredAsset !== 'undefined' && requiredAsset.refDate === today) {
-//       warn.log(`Skipping already updated asset '${requiredAsset.code}'`)
-//       continue
-//     }
+  const errors: ScrapError[] = []
+  let somethingChanged = false
+  for await (const requiredAsset of assets) {
+    if (typeof requiredAsset !== 'undefined' && requiredAsset.refDate === today) {
+      warn.log(`Skipping already updated asset '${requiredAsset.code}'`)
+      continue
+    }
 
-//     try {
-//       await page.goto(`${baseUrl}/${requiredAsset.code}/caracteristicas`)
-//     } catch (error) {
-//       err.log(`Couldn't get the results for the asset '${requiredAsset.code}'. Make sure its code is correct`)
-//       if (error instanceof Error) err.log(error.message)
-//       else err.log(error)
-//       continue
-//     }
+    try {
+      await page.goto(`${debenturesBaseUrl}/${requiredAsset.code}/caracteristicas`)
+      await page.setViewport({ width: 1080, height: 1024 })
+      info.log(`💰 Scraping for the nominal price of '${requiredAsset.code}'`)
+      try {
+        await page.waitForSelector('.lower-card-item-value')
+      } catch (error) {
+        await page.waitForSelector('#maskNotFound', { timeout: 1000 })
+        try {
+          await page.goto(`${certificateOfReceivableBaseUrl}/${requiredAsset.code}/caracteristicas`)
+          await page.waitForSelector('.lower-card-item-value')
+        } catch (error) {
+          err.log(`Couldn't get the results for the asset '${requiredAsset.code}'. Make sure its code is correct`)
+          if (error instanceof Error) err.log(error.message)
+          else err.log(error)
+          continue
+        }
+      }
+    } catch (error) {
+      err.log(`Couldn't get the results for the asset '${requiredAsset.code}'. Make sure its code is correct`)
+      if (error instanceof Error) err.log(error.message)
+      else err.log(error)
+      continue
+    }
 
-//     await page.setViewport({ width: 1080, height: 1024 })
+    let gotTheValue = false
+    for await (const el of (await page.$$('.lower-card-item'))) {
+      // Find the title and extract the refDate
+      let refDate = await el.$eval('.lower-card-item-title', (title) => {
+        if (title.textContent !== null && title.textContent.includes('PU PAR')) {
+          return title.textContent.replace(/\n/g, ' ').replace(/.*ref\. +([\d/]+).*/, '$1')
+        } else return undefined
+      })
 
-//     info.log(`💰 Scraping for the nominal price of '${requiredAsset.code}'`)
-//     await page.waitForSelector('.lower-card-item-value')
-//     let gotTheValue = false
-//     for await (const el of (await page.$$('.lower-card-item'))) {
-//       // Find the title and extract the refDate
-//       let refDate = await el.$eval('.lower-card-item-title', (title) => {
-//         if (title.textContent !== null && title.textContent.includes('PU PAR')) {
-//           return title.textContent.replace(/\n/g, ' ').replace(/.*ref\. +([\d/]+).*/, '$1')
-//         } else return undefined
-//       })
-
-//       // If there's refDate, get the new value
-//       if (typeof refDate !== 'undefined') {
-//         refDate = DateTime.fromFormat(refDate, 'dd/MM/yyyy').toFormat('yyyy-MM-dd')
-//         const newValue = (await el.$eval('.lower-card-item-value', (value) => value.textContent ?? value.textContent))
-//         if (typeof newValue === 'string') {
-//           gotTheValue = true
-//           // Update value if already exist
-//           info.log(`🔄 Updating asset => ${requiredAsset.code}: ${newValue} (${refDate})`)
-//           requiredAsset.value = newValue
-//           requiredAsset.refDate = refDate
-//           somethingChanged = true
-//         } else {
-//           err.log(`Unexpected nominal price for '${requiredAsset.code}'`)
-//         }
-//         break
-//       }
-//     }
-//     if (!gotTheValue) {
-//       err.log(`Couldn't get PU PAR title in the page for '${requiredAsset.code}'`)
-//     }
-//   }
-//   await browser.close()
-//   return somethingChanged
-// }
+      // If there's refDate, get the new value
+      if (typeof refDate === 'string') {
+        refDate = DateTime.fromFormat(refDate, 'dd/MM/yyyy').toFormat('yyyy-MM-dd')
+        let newValue = (await el.$eval('.lower-card-item-value', (value) => value.textContent ?? value.textContent))
+        if (typeof newValue === 'string') {
+          gotTheValue = true
+          newValue = newValue.replace('R$ ', '')
+          // Update value if already exist
+          info.log(`🔄 Updating asset => ${requiredAsset.code}: ${newValue} (${refDate})`)
+          requiredAsset.value = newValue
+          requiredAsset.refDate = refDate
+          somethingChanged = true
+        } else {
+          const message = `Unexpected nominal price for '${requiredAsset.code}'`
+          err.log(message)
+          errors.push(new ScrapError(message, requiredAsset.code))
+        }
+        break
+      }
+    }
+    if (!gotTheValue) {
+      const message = `Couldn't get PU PAR title in the page for '${requiredAsset.code}'`
+      err.log(message)
+      errors.push(new ScrapError(message, requiredAsset.code))
+    }
+  }
+  await browser.close()
+  return [somethingChanged, errors]
+}
 
 /**
  * Updates assets using gaxios
  * @param assets the assets to update
  * @returns whether any assets was updated
  */
-async function gaxiosScraper(assets: Asset[]): Promise<[boolean, Error[]]> {
+async function gaxiosScraper(assets: Asset[]): Promise<[boolean, ScrapError[]]> {
   const today = DateTime.now().setZone('America/New_York')
   const todayString = today.toFormat('yyyy-MM-dd')
   if (typeof assets.find(requiredAsset => requiredAsset.refDate !== todayString) === 'undefined') {
@@ -99,7 +121,7 @@ async function gaxiosScraper(assets: Asset[]): Promise<[boolean, Error[]]> {
 
   const baseUrl = atob(atob('YUhSMGNITTZMeTkzZDNjdVpHVmlaVzUwZFhKbGN5NWpiMjB1WW5JdlpYaHdiRzl5Wlc5emJtUXZZMjl1YzNWc2RHRmhaR0ZrYjNNdlpXMXBjM052WlhOa1pXUmxZbVZ1ZEhWeVpYTXZjSFZvYVhOMGIzSnBZMjlmY2k1aGMzQQ'))
 
-  const errors: Error[] = []
+  const errors: ScrapError[] = []
   let somethingChanged = false
   const lastWeekString = today.minus({ week: 1 }).toFormat('dd/MM/yyyy').replace('/', '%2F')
   const nextWeekString = today.plus({ week: 1 }).toFormat('dd/MM/yyyy').replace('/', '%2F')
@@ -121,7 +143,7 @@ async function gaxiosScraper(assets: Asset[]): Promise<[boolean, Error[]]> {
       if (typeof response.data !== 'string') {
         const message = `Unknown data type for the asset '${requiredAsset.code}': ${response.data as string}`
         err.log(message)
-        errors.push(new Error(message))
+        errors.push(new ScrapError(message, requiredAsset.code))
         continue
       }
 
@@ -131,46 +153,46 @@ async function gaxiosScraper(assets: Asset[]): Promise<[boolean, Error[]]> {
       const dom = parseFromString(response.data)
       const assetCell = dom.getElementsByTagName('td').find(e => e.textContent === requiredAsset.code)
       if (typeof assetCell === 'undefined') {
-        const message = `Couldn't find a table with the element '${requiredAsset.code}'`
-        err.log(message)
-        errors.push(new Error(message))
+        const message = `Couldn't find a table with the element '${requiredAsset.code}'. Will try again...`
+        warn.log(message)
+        errors.push(new ScrapError(message, requiredAsset.code))
         continue
       }
       const parentCell = assetCell.parentNode
       if (parentCell === null) {
-        const message = `Couldn't find a table with the element '${requiredAsset.code}'`
-        err.log(message)
-        errors.push(new Error(message))
+        const message = `Couldn't find a table with the element '${requiredAsset.code}'. Will try again...`
+        warn.log(message)
+        errors.push(new ScrapError(message, requiredAsset.code))
         continue
       }
       const refDateCell = parentCell.childNodes[1]
       if (refDateCell === null || typeof refDateCell === 'undefined') {
-        const message = `The parent cell of the row should have childNodes[1] for asset '${requiredAsset.code}'`
-        err.log(message)
-        errors.push(new Error(message))
+        const message = `The parent cell of the row should have childNodes[1] for asset '${requiredAsset.code}'. Will try again...`
+        warn.log(message)
+        errors.push(new ScrapError(message, requiredAsset.code))
         continue
       }
       const refDate = DateTime.fromFormat(refDateCell.textContent, 'dd/MM/yyyy')
       if (!refDate.isValid) {
-        const message = `Unexpected date format '${refDateCell.textContent}' for asset '${requiredAsset.code}'`
-        err.log(message)
-        errors.push(new Error(message))
+        const message = `Unexpected date format '${refDateCell.textContent}' for asset '${requiredAsset.code}'. Will try again...`
+        warn.log(message)
+        errors.push(new ScrapError(message, requiredAsset.code))
         continue
       }
       const refDateString = refDate.toFormat('yyyy-MM-dd')
 
       const valueCell = parentCell.childNodes[5]
       if (valueCell === null || typeof valueCell === 'undefined') {
-        const message = `The parent cell of the row should have childNodes[5] for asset '${requiredAsset.code}'`
-        err.log(message)
-        errors.push(new Error(message))
+        const message = `The parent cell of the row should have childNodes[5] for asset '${requiredAsset.code}'. Will try again...`
+        warn.log(message)
+        errors.push(new ScrapError(message, requiredAsset.code))
         continue
       }
       const value = valueCell.textContent
       if (value.match(/^\d{1,3}(\.\d{3})*,\d+/) === null) {
-        const message = `Unexpected value format '${value}' for asset '${requiredAsset.code}'`
-        err.log(message)
-        errors.push(new Error(message))
+        const message = `Unexpected value format '${value}' for asset '${requiredAsset.code}'. Will try again...`
+        warn.log(message)
+        errors.push(new ScrapError(message, requiredAsset.code))
         continue
       }
 
@@ -181,11 +203,11 @@ async function gaxiosScraper(assets: Asset[]): Promise<[boolean, Error[]]> {
 
       somethingChanged = true
     } catch (error) {
-      let message = `Couldn't get the results for the asset '${requiredAsset.code}'. Make sure its code is correct`
+      let message = `Couldn't get the results for the asset '${requiredAsset.code}'. Make sure its code is correct. Will try again...`
       if (error instanceof Error) message = `${message}: ${error.message}`
       else message = `${message}: ${error as string}`
-      err.log(message)
-      errors.push(new Error(message))
+      warn.log(message)
+      errors.push(new ScrapError(message, requiredAsset.code))
       continue
     }
   }
